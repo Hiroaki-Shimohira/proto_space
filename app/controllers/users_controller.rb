98d05ccf4bd51c804aeb936c1ba40be1d3aad7ae
下平ヒロアキ <s.hiro.w0w1995@gmail.com>
2016-08-18 21:55:11 +0900

    git trable test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,15 @@ class UsersController < ApplicationController
 	def show
 		@user = User.find(params[:id])
 	end
+	def edit
+		@user = User.find(params[:id])
+	end
+	def update
+		current_user.update(update_params)
+		redirect_to "/"
+	end
+	private
+	def update_params
+		params.require(:user).permit(:nickname, :email, :member, :profile, :works)
+	end
 end
