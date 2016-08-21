213d5726e46c574aac0e723397212f65af80e641
下平ヒロアキ <s.hiro.w0w1995@gmail.com>
2016-08-21 22:16:35 +0900

    fixed and make flash massage function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,24 @@
 class UsersController < ApplicationController
+	before_action :get_user, only: [:show, :edit]
 	def show
-		@user = User.find(params[:id])
 	end
 	def edit
-		@user = User.find(params[:id])
 	end
 	def update
-		current_user.update(update_params)
-		redirect_to "/"
+		if current_user.update(update_params)
+			redirect_to(root_path)
+			flash[:success] = "成功"
+		else
+			redirect_to(edit_user_path)
+			flash[:warning] = "失敗"
+		end
 	end
+
 	private
 	def update_params
 		params.require(:user).permit(:nickname, :email, :member, :profile, :works)
+	end
+	def get_user
+		@user = User.find(params[:id])
 	end
 end
