6c98014a66d66763b37f45b4a597532d5bbcb387
下平ヒロアキ <s.hiro.w0w1995@gmail.com>
2016-08-22 14:13:41 +0900

    do rails g model prototype and rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160803044600) do
+ActiveRecord::Schema.define(version: 20160822050409) do
+
+  create_table "prototypes", force: :cascade do |t|
+    t.string   "title",      limit: 255
+    t.string   "catchcopy",  limit: 255
+    t.text     "concept",    limit: 65535
+    t.integer  "user_id",    limit: 4
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255,   default: "", null: false
