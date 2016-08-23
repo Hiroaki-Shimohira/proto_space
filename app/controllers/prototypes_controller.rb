af79db2411abeb6e71c178dcafe40c8920fab41c
下平ヒロアキ <s.hiro.w0w1995@gmail.com>
2016-08-23 14:31:52 +0900

    make view with form_for

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -4,5 +4,13 @@ class PrototypesController < ApplicationController
 	def show
 	end
 	def new
+		@prototype = Prototype.new
+	end
+	def create
+		binding.pry
+		Prototype.create(create_params)
+	end
+	private
+	def create_params
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   root 'prototypes#index'
   devise_for :users
   resources :users, only: [:show, :edit, :update]
+  resources :prototypes, only: [:new, :create]
 
 
-  get  '/prototypes/index'  =>    'prototypes#index'
-  get  '/prototypes/new'    =>    'prototypes#new'
-  get  '/prototypes/show'   =>    'prototypes#show'
+  # get  '/prototypes/index'  =>    'prototypes#index'
+  # get  '/prototypes/new'    =>    'prototypes#new'
+  # get  '/prototypes/show'   =>    'prototypes#show'
 end
