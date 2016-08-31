3950ac2e40802c9b1a3d3a63ee36a95f41702c27
下平ヒロアキ <s.hiro.w0w1995@gmail.com>
2016-08-31 13:40:51 +0900

    edit to display date

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
 	before_action :get_user, only: [:show, :edit]
 	def show
-		@user = User.find(params[id])
+		@user = User.find(params[:id])
+		@user_proto= @user.prototypes
 	end
 	def edit
 	end
