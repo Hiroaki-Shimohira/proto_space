5a8fe067c73ed6c10a0869af38b84d562d8c5f53
下平ヒロアキ <s.hiro.w0w1995@gmail.com>
2016-09-01 20:19:48 +0900

    delete comment-out

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,4 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:show, :edit, :update]
   resources :prototypes, only: [:new, :create, :show]
-
-
-  # get  '/prototypes/index'  =>    'prototypes#index'
-  # get  '/prototypes/new'    =>    'prototypes#new'
-  # get  '/prototypes/show'   =>    'prototypes#show'
 end
