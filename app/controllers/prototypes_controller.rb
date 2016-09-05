33620d0dfcc48f2772794e857aec0d9f1632bf0a
下平ヒロアキ <s.hiro.w0w1995@gmail.com>
2016-09-05 09:41:59 +0900

    fix

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,10 +1,10 @@
 class PrototypesController < ApplicationController
 	before_action :authenticate_user!, except: [:index, :show]
+	before_action :create_prototype_instance, only: [:show, :edit, :update, :destroy]
 	def index
 		@prototypes = Prototype.order(id: :ASC).page(params[:page]).per(8)
 	end
 	def show
-		@prototype = Prototype.find(params[:id])
 	end
 	def new
 		@prototype = Prototype.new
@@ -13,8 +13,8 @@ class PrototypesController < ApplicationController
 	def create
 
 
-		@prototype = current_user.prototypes.new(create_params)
-		if @prototype.save(create_params)
+		@prototype = current_user.prototypes.new(prototype_params)
+		if @prototype.save(prototype_params)
 			redirect_to(root_path)
 			flash[:success] = "いいね！"
 		else
@@ -24,28 +24,25 @@ class PrototypesController < ApplicationController
 
 	end
 	def edit
-		@prototype = Prototype.find(params[:id])
 		@prototype.capture_images.build
 	end
 	def update
-		prototype = Prototype.find(params[:id])
-		prototype.update(prototype_params)
+		@prototype.update(prototype_params)
     	redirect_to(root_path)
 	end
 	def destroy
-		prototype = Prototype.find(params[:id])
-		if prototype.user_id == current_user.id
-			prototype.destroy
+		if @prototype.user_id == current_user.id
+			@prototype.destroy
 			redirect_to(root_path)
 		else
 			redirect_to(root_path)
 			flash[:warning] = "ちゃうやろ"
 		end
 	end
-	private
-	def create_params
-		params.require(:prototype).permit(:title, :catchcopy, :concept, capture_images_attributes: [:image,:satus])
+	def create_prototype_instance
+		@prototype = Prototype.find(params[:id])
 	end
+	private
 	def prototype_params
 		params.require(:prototype).permit(:title, :catchcopy, :concept, capture_images_attributes: [:image,:satus,:id])
 	end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root 'prototypes#index'
   devise_for :users
   resources :users, only: [:show, :edit, :update]
-  resources :prototypes, only: [:new, :create, :show, :edit, :destroy, :update]
+  resources :prototypes
 end
