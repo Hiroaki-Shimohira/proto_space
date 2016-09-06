5e95ad6ac7d46f54ca611cc48a6d11bcac950208
下平ヒロアキ <s.hiro.w0w1995@gmail.com>
2016-09-06 14:55:36 +0900

    make likes table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160825090809) do
+ActiveRecord::Schema.define(version: 20160906054633) do
 
   create_table "capture_images", force: :cascade do |t|
     t.text     "image",        limit: 65535
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20160825090809) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "satus",        limit: 4,     default: 0
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer  "user_id",      limit: 4
+    t.integer  "prototype_id", limit: 4
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "prototypes", force: :cascade do |t|
