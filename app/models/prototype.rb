72239e037d887079f958f9b4652eb2c1b095d1f9
下平ヒロアキ <s.hiro.w0w1995@gmail.com>
2016-09-06 20:10:44 +0900

    like function

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,6 +1,10 @@
 class Prototype < ActiveRecord::Base
 	belongs_to :user
 	has_many :capture_images
+	has_many :likes, dependent: :destroy
 	accepts_nested_attributes_for :capture_images
 	validates :title, presence: true
+	def like_count(user_id)
+		likes.find_by(user_id: user_id)
+	end
 end
