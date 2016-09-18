8bb9fc31256628996df069b61a3168ee2265915c
下平ヒロアキ <s.hiro.w0w1995@gmail.com>
2016-09-18 17:28:55 +0900

    edit tags/index.html.haml

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,7 +1,9 @@
 class TagsController < ApplicationController
 	def index
-		# Prototype.tag_counts
+		@tags = ActsAsTaggableOn::Tag.all
 	end
 	def show
+		@tag =  ActsAsTaggableOn::Tag.find(params[:id])
+    	@prototypes = Prototype.tagged_with(@tag.name)
 	end
 end
