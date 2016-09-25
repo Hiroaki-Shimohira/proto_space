6548205407320a03605fbfd066555603506e2d87
下平ヒロアキ <s.hiro.w0w1995@gmail.com>
2016-09-25 12:23:57 +0900

    edit prototypes.coffee

--- app/assets/javascripts/preview-image.js
+++ app/assets/javascripts/preview-image.js
@@ -1,16 +1,16 @@
-window.addEventListener( 'load', function() {
-    for ( num = 1; num < 5; num++ ) {
-        ( function( num ) { // 即時関数
-            document.getElementById( 'file' + num ).addEventListener( 'change', function() {
-                var fileList = this.files;
-                var type = fileList.type;
-                var i = 0;
-                var fileReader = new FileReader();
-                fileReader.onload = function() {
-                    document.getElementById( 'js-pre-image' + num ).src = this.result;
-                }
-                fileReader.readAsDataURL( fileList[ i ] );
-            } );
-        } )( num ); // 即時関数
-    }
-} );
+// window.addEventListener( 'load', function() {
+//     for ( num = 1; num < 5; num++ ) {
+//         ( function( num ) { // 即時関数
+//             document.getElementById( 'file' + num ).addEventListener( 'change', function() {
+//                 var fileList = this.files;
+//                 var type = fileList.type;
+//                 var i = 0;
+//                 var fileReader = new FileReader();
+//                 fileReader.onload = function() {
+//                     document.getElementById( 'js-pre-image' + num ).src = this.result;
+//                 }
+//                 fileReader.readAsDataURL( fileList[ i ] );
+//             } );
+//         } )( num ); // 即時関数
+//     }
+// } );
--- app/assets/javascripts/prototypes.coffee
+++ app/assets/javascripts/prototypes.coffee
@@ -1,3 +1,16 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
+window.addEventListener( 'load', ->
+  for num in [1..4]
+    do (num) ->
+      document.getElementById( 'file' + num ).addEventListener( 'change', ->
+        fileList = this.files
+        type = fileList.type
+        i = 0
+        fileReader = new FileReader()
+        fileReader.onload = ->
+          document.getElementById( 'js-pre-image' + num ).src = this.result;
+        fileReader.readAsDataURL( fileList[ i ] );
+      );
+);
